cca536318d9d62622c1ccdae03d2f51ff9a97274
tsellam <thibault.sellam@gmail.com>
2016-06-06 17:54:43 +0200

    Factored out settings and constants

---
+++ R/settings.R
@@ -0,0 +1,8 @@
+#############
+# Constants #
+#############
+# Dependency calculation functions for each type of column
+DEP_FUNC_NUM <- cor_matrix
+DEP_FUNC_CAT <- cramerV_matrix
+
+P_VALUE_ZIG <- 0.05
